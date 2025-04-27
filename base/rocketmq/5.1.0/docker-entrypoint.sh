c171b67a38867fbd77636146636ab39a941961e7
alexanderwangamja <thkim3174@gmail.com>
2025-04-28 00:08:41 +0900

    Clean up unnecessary files, keep only Flask SSTI PoC

--- base/rocketmq/5.1.0/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-ROCKETMQ_HOME="$(pwd)"
-if [[ "$1" == mq* ]]; then
-    exec "$ROCKETMQ_HOME/bin/$1" "${@:2}"
-else
-    exec "$@"
-fi
--- imagemagick/imagetragick/README.md
+++
@@ -1,3 +0,0 @@
-# Page Moved
-
-The "imagetragick" issue is moved to [CVE-2016-3714](../CVE-2016-3714)
